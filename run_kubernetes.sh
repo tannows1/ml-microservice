96f583b1cf2922f238051882a70808944ce116de
tannows1 <tannows@outlook.com>
2020-07-13 11:23:50 -0500

    Fill in todo's

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="tannows1/ml-microservice:latest"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-microservice --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment ml-microservice: --type=LoadBalancer --port=8000 --target-port=80
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="tannows1/ml-microservice:latest"
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
+docker login
+docker tag ml-microservice:latest $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
